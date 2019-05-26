5f87544cb442ba9de72fa33b80fb5590abbf53a0
pawan rana <pawan.pvt.ltd@gmail.com>
2019-05-26 16:47:09 +0530

    Updates

---
+++ precache-manifest.b664e4f4204ca6a347d140d719642d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d70534950b6f7188ddaa560847322ae6",
+    "url": "/Commutatus-test/index.html"
+  },
+  {
+    "revision": "1a2e6e87d620732e4c08",
+    "url": "/Commutatus-test/static/css/main.67f06700.chunk.css"
+  },
+  {
+    "revision": "0c4feb38a1632660cf01",
+    "url": "/Commutatus-test/static/js/2.fa0004aa.chunk.js"
+  },
+  {
+    "revision": "1a2e6e87d620732e4c08",
+    "url": "/Commutatus-test/static/js/main.8133b95d.chunk.js"
+  },
+  {
+    "revision": "dbac0f13630985895d70",
+    "url": "/Commutatus-test/static/js/runtime~main.6754c431.js"
+  },
+  {
+    "revision": "f66ac276352d5428703f899fba6cae3a",
+    "url": "/Commutatus-test/static/media/language.f66ac276.csv"
+  }
+]);
